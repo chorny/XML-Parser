3d425855b1f0c1067f0c5bf8418d392097f71e1f
matt <matt@2154792d-645b-41ca-96ca-efebb8c93226>
2007-11-20 14:23:04 +0000

    2.36 changes
    git-svn-id: svn://axkit.org/XML-Parser/trunk@23 2154792d-645b-41ca-96ca-efebb8c93226

--- Parser.pm
+++ Parser.pm
@@ -12,7 +12,7 @@ use Carp;
 
 BEGIN {
   require XML::Parser::Expat;
-  $VERSION = '2.35';
+  $VERSION = '2.36';
   die "Parser.pm and Expat.pm versions don't match"
     unless $VERSION eq $XML::Parser::Expat::VERSION;
 }
